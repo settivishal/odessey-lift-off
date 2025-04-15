d65b2c3598246a3ae614699701dc53eb304bfaf9
settivishal <vishalsetti2000@gmail.com>
2025-04-15 00:15:55 -0400

    updated index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./schema.js";
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const {url} = await startStandaloneServer(server);
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+startApolloServer().catch((error) => {
+  console.error("Error starting Apollo Server:", error);
+  process.exit(1);
+});
