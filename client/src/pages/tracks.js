c89132e1e85c78c7bcb8a3916a830459507dec3b
settivishal <vishalsetti2000@gmail.com>
2025-04-15 23:41:16 -0400

    updated tracks

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
 import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 const TRACKS = gql`
   query GetTracks {
@@ -25,12 +27,19 @@ const TRACKS = gql`
  */
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
-  console.log("data", data);
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
 
-  return <Layout grid>{JSON.stringify(data)}</Layout>
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
