0c530ca8bbf3b7c4b2620027c599459a0ae186ee
settivishal <vishalsetti2000@gmail.com>
2025-04-16 11:38:27 -0400

    updated index to send DataSources

--- server/src/index.js
+++ server/src/index.js
@@ -1,39 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import typeDefs from "./schema.js";
-
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+import resolvers from "./resolvers.js";
+import TrackAPI from "./track-api.js";
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
-    }),
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
   console.log(`🚀  Server ready at: ${url}`);
 }
 
--- server/src/track-api.js
+++ server/src/track-api.js
@@ -1,4 +1,4 @@
-import { RESTDataSource } from 'apollo-datasource-rest';
+import { RESTDataSource } from '@apollo/datasource-rest';
 
 class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
